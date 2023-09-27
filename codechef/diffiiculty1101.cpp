29f403bf6bb705018224194fc0b7c6d7e9d4214c
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-09-27 23:22:54 +0530

    Update diffiiculty1101.cpp

--- codechef/diffiiculty1101.cpp
+++ codechef/diffiiculty1101.cpp
@@ -1,3 +1,22 @@
+//In a regular table tennis match, the player who serves changes every time after 2 points are scored, regardless of which players scored them.
+
+// Chef and Cook are playing a different match — they decided that the player who serves would change every time after 
+// �
+// K points are scored instead (again regardless of which players scored them). When the game starts, it's Chef's turn to serve.
+
+// You are given the current number of points scored by Chef and Cook (
+
+// ​
+//   and 
+// �
+// 2
+// P 
+// 2
+// ​
+//   respectively). Find out whether Chef or Cook has to serve next.///
+
+
+
 #include <iostream>
 using namespace std;
 
