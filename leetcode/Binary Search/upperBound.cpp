45cab7b64053134733e914f381d76f40aa220562
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-09-27 20:21:53 +0530

    Update upperBound.cpp

--- leetcode/Binary Search/upperBound.cpp
+++ leetcode/Binary Search/upperBound.cpp
@@ -1,31 +1,15 @@
+int upperBound(vector<int> &arr, int x, int n){
+	int start =0;
+	int end =n-1;
+	int answer =n;
+	while(start<=end){
+		int middle = (start+end)/2;
+		if(arr[middle]>x){
+			answer = middle;
 
-#include <bits/stdc++.h>
-using namespace std;
-
-int lowerBound(vector<int> arr, int n, int x) {
-    int low = 0, high = n - 1;
-    int ans = n;
-
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        // maybe an answer
-        if (arr[mid] >= x) {
-            ans = mid;
-            //look for smaller index on the left
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1; // look on the right
-        }
-    }
-    return ans;
-}
-
-int main()
-{
-    vector<int> arr = {3, 5, 8, 15, 19};
-    int n = 5, x = 9;
-    int ind = lowerBound(arr, n, x);
-    cout << "The lower bound is the index: " << ind << "\n";
-    return 0;
+			end = middle-1;
+		}else{
+			start = middle+1;
+		}
+	}return answer;	
 }
