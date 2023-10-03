bb8ef410770d255e39cf6d0677575e4da23eaf23
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-10-03 15:13:36 +0530

    Create difficulty1106.cpp

---
+++ codechef/difficulty1106.cpp
@@ -0,0 +1,16 @@
+//question link = "https://www.codechef.com/problems/S01E01?tab=statement"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n<21) cout<<"no\n";
+	    else cout<<"yes\n";
+	}
+	return 0;
+}
