9b03461b96a1d4d790a88ae616c501983559bb6a
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-10-04 13:15:12 +0530

    Update PeakElement.cpp

--- leetcode/Binary Search/PeakElement.cpp
+++ leetcode/Binary Search/PeakElement.cpp
@@ -1,4 +1,9 @@
-/*so what we do here is we check if mid is peak and if not we go towards higher element to check*/
+/* question - A peak element is an element that is strictly greater than its neighbors.
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+You must write an algorithm that runs in O(log n) time.*/
+
+//so what we do here is we check if mid is peak and if not we go towards higher element to check
 
 class Solution {
 public:
