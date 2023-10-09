a0de0e1a5a2b8fbb509acd5828d40e38850d4232
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-10-09 21:09:46 +0530

    Update RemoveBadElement.cpp

--- codechef/RemoveBadElement.cpp
+++ codechef/RemoveBadElement.cpp
@@ -14,7 +14,7 @@ int main() {
         for (int i = 0; i < n; i++)
             hash[arr[i]]++;
       
-        // find the max frequency
+        
         int max_count = 0, res = -1;
         for (auto i : hash) {
             if (max_count < i.second) {
