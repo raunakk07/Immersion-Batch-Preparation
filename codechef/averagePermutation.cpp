7606f68734aa66b0856c25f79abd8284c97c569d
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-10-16 21:16:20 +0530

    Update averagePermutation.cpp

--- codechef/averagePermutation.cpp
+++ codechef/averagePermutation.cpp
@@ -1,1 +1,24 @@
 //question link = "https://www.codechef.com/problems/AVGPERM?tab=statement"
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    vector<int> v;
+	    v.push_back(n-1);
+	    v.push_back(n-2);
+	    for(int i =1;i<n-3;i++){
+	        v.push_back(i);
+	    }if(n>3) v.push_back(n-3);
+	    v.push_back(n);
+	    for(auto it: v){
+	        cout<<it<<" ";
+	    }cout<<endl;
+	}
+	return 0;
+}
