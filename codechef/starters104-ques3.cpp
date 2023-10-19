387e51ced72b39088fdc147f955f21f9dcff130a
Harshit Srivastava <91839831+raunakk07@users.noreply.github.com>
2023-10-19 21:48:49 +0530

    Create starters104-ques3.cpp

---
+++ codechef/starters104-ques3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int choc[n];
+	    for(int i =0;i<n;i++) cin>>choc[i];
+	    int sum =0;
+	    for(int i =0;i<n;i++) sum+=choc[i];
+	    
+	    if(sum<n) cout<<"no\n";
+	    
+	    else{
+	        sum = sum%n;
+	        if(k == 0 && sum != 0 ) cout<<"no\n";
+	        else cout<<"yes\n";
+	    }
+	    
+	}
+	return 0;
+}
